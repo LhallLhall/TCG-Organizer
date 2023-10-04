6fd8e4472bf82a679d5bbb8429d6c167584a539e
Logan Hall <loganhall@Logans-Air.localdomain>
2023-10-04 16:10:53 -0400

    added new visuals for the data

--- tcg-organizer/src/app/page.tsx
+++ tcg-organizer/src/app/page.tsx
@@ -3,8 +3,8 @@
 // import styles from "./page.module.css";
 import Axios from "axios";
 import React, { useState, useEffect } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
@@ -13,14 +13,14 @@ export default function Home() {
   const [searchValue, setSearchValue] = useState("");
   let inputField = document.getElementById("inputField");
 
-  const search = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+  const search = (e: { target: { value: React.SetStateAction<string> } }) => {
     setSearchValue(e.target.value);
     console.log(e.target.value);
   };
 
   const submit = () => {
-    if(searchValue !== ""){
-      try{
+    if (searchValue !== "") {
+      try {
         Axios.get(
           `https://api.magicthegathering.io/v1/cards?name=${searchValue}`
         ).then((response) => {
@@ -29,15 +29,14 @@ export default function Home() {
           console.log(response.data.cards);
           // console.log(cardData, "first data");
         });
-      }catch{
+      } catch {
         toast.error("Search Was Invalid");
-        position: toast.POSITION.TOP_CENTER
+        position: toast.POSITION.TOP_CENTER;
       }
     } else {
       toast.error("Search is empty");
-      position: toast.POSITION.TOP_CENTER
+      position: toast.POSITION.TOP_CENTER;
     }
-    
   };
 
   console.log(cardData);
@@ -49,39 +48,42 @@ export default function Home() {
       originalText = originalText.replace(/{T}, /g, "");
     }
     return (
-        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
+      <div key={i} className="">
+        <li className="list-group-item">
           <strong>{(card as any).name}</strong> - {(card as any).setName}
-        </a>
-      // <div className="col-6 col-s-4 col-m-3" key={Date.now()}>
-      //   <div className="card" style={{ width: "18rem" }}>
-      //     <img src={(card as any).imageUrl} className="card-img-top" alt="..." />
-      //     <div className="card-body">
-      //       <h5 className="card-title">{(card as any).name}</h5>
-      //       <p className="card-text">{originalText}</p>
-      //       <a href="#" className="btn btn-primary">
-      //         Go somewhere
-      //       </a>
-      //     </div>
-      //   </div>
-      // </div>
+        </li>
+      </div>
     );
   });
+  // <div id={"item" + i} className="collapse">
+  //   <div className="row">
+  //     <div className="col-6 d-flex align-items-center justify-content-center">
+  //       <img src={(card as any).imageUrl} className="" alt="..." />
+  //     </div>
+  //     <div className="col-6 d-flex justify-content-center">
+  //       <p>{originalText}</p>
+  //     </div>
+  //   </div>
+  // </div>
   //! This is the main return
   return (
     <main style={{ height: "100vh", width: "100vw" }}>
       <div className="container w-100 h-100">
         <div className="">
           <input type="text" onChange={search} id="inputField" />
-          <button className="btn btn-light" onClick={submit}>Search</button>
+          <button className="btn btn-light" onClick={submit}>
+            Search
+          </button>
         </div>
 
         <div className="row">
-          <div className="list-group">
-            {mappedData}
-          </div> 
+          <div className="col-6">
+            <div className="list-group">{mappedData}</div>
           </div>
+          <div className="col-6">{/* images go here */}</div>
+        </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </main>
   );
 }
@@ -97,3 +99,29 @@ export default function Home() {
   <a class="list-group-item list-group-item-action disabled">A disabled link item</a>
 </div> 
 */
+
+/* <a href="#" className="list-group-item list-group-item-action dropdown-toggle" aria-current="true" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <strong>{(card as any).name}</strong> - {(card as any).setName}
+        </a>
+        <div className="dropdown-menu">
+          <p className="">{originalText}</p>
+          <img
+            src={(card as any).imageUrl}
+            className="card-img-top"
+            alt="..."
+            width={"370px"}
+            height={"265px"}
+          />
+        </div> */
+// <div className="col-6 col-s-4 col-m-3" key={Date.now()}>
+//   <div className="card" style={{ width: "18rem" }}>
+//     <img src={(card as any).imageUrl} className="card-img-top" alt="..." />
+//     <div className="card-body">
+//       <h5 className="card-title">{(card as any).name}</h5>
+//       <p className="card-text">{originalText}</p>
+//       <a href="#" className="btn btn-primary">
+//         Go somewhere
+//       </a>
+//     </div>
+//   </div>
+// </div>
